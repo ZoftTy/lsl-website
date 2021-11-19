aad8d545b553206905aec8889e08a584b89b7789
ZoftTy <poledye@icloud.com>
2021-11-19 15:27:51 +0800

    update WorkingTogether Data

--- src/views/WorkingTogether.vue
+++ src/views/WorkingTogether.vue
@@ -47,7 +47,7 @@ onMounted(() => {
     <HeaderImageLayout>
       <!-- 图片 -->
       <template v-slot:image>
-        <img src="/images/education-system-header.png" alt="" />
+        <img src="/images/education-system-header.png" alt />
       </template>
 
       <!-- 菜单选择器 -->
@@ -72,9 +72,14 @@ onMounted(() => {
     </div>
 
     <!-- 校企合作 -->
-    <div class="max-width-wrap" v-if="route.params.category == 'enterprise'">
-      <el-image :src="data.enterprise.icon"></el-image>
-      <p>{{ data.enterprise.content }}</p>
+    <div class="max-width-wrap enterprise" v-if="route.params.category == 'enterprise'">
+      <el-image class="image-wrap" :src="data.enterprise.icon"></el-image>
+      <h4>（一）酒店服务类</h4>
+      <p>广州合兴大酒店有限公司、广东湖景金阁饮食有限公司、广州花都竹缘美食、广州金骏酒店有限公司、广州金联福餐饮有限公司、广州民航大酒店、广州福荟来餐饮服务有限公司广州豪苑酒店有限公司、广州华钜君悦酒店有限公司、中南民航经济发展公司芙蓉会议中心</p>
+      <h4>（二）化妆品轻工业类</h4>
+      <p>广州一兆无纺日化有限公司、广州喜悦生物科技有限公司、丹姿集团（广州市科能化妆品科研有限公司）、广州艾蓓生物科技有限公司</p>
+      <h4>（三）电子制造业类</h4>
+      <p>广州科林电器有限公司、佛山鋐利电子有限公司、步步高教育电子有限公司、东莞清溪富崴电子厂、光宝电子（广州）有限公司、广东新宝电器股份有限公司、东莞欧珀精密电子有限公司、广州市尊浪电器有限公司、东莞库柏电子有限公司、深圳思纬市场资讯有限公司广州分公司、佛山顺德北滘塑料电器有限公司、比亚迪股份有限公司、广东泛仕达机电有限公司、广州南联航空食品有限公司、中山TCL制冷设备有限公司、海信广东空调有限公司、佛山振宣精密科技有限公司、广州市亿丰彩印包装有限公司、广州市博泰光学科技有限公司、广州帅著运动用品有限公司、上海苏博眼镜有限公司、广州奥博文具有限公司、广州市恒俊生态农业发展有限公司、广东云山仓储服务有限公司、广州碧源管业有限公司、广州市佳宏电子有限公司</p>
     </div>
   </div>
 </template>
@@ -102,6 +107,26 @@ onMounted(() => {
     &:nth-last-child(1) {
       margin-bottom: 0;
     }
+  }
+}
+
+.enterprise {
+  padding: 50px 20px 60px 20px;
+  text-align: center;
+
+  .image-wrap {
+    width: 50%;
+
+    margin-bottom: 20px;
+  }
+  h4 {
+    margin-bottom: 12px;
+
+    font: @title-5 normal;
+    text-align: initial;
+  }
+  p {
+    text-align: initial;
   }
 }
 </style>
