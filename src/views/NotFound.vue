e6083304258dd9b7dafa33a6734949498b99a6ff
ZoftTy <poledye@icloud.com>
2021-11-20 16:16:26 +0800

    update 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,15 +1,40 @@
 <script setup>
+import { pushLink } from '../utils/router';
+
 </script>
 
 <template>
-  <div class="not-found">404 Not Found</div>
+  <div class="not-found">
+    <h1>404 Not Found</h1>
+    <el-button @click="pushLink('/')">返回首页</el-button>
+  </div>
 </template>
 
 <style lang="less" scoped>
 .not-found {
-  padding: 100px;
+  width: 100vw;
+  height: calc(100vh - var(--header-height));
 
-  font-size: 100px;
+  padding: 20px;
+
   text-align: center;
+
+  & > * {
+    margin-bottom: 4vh;
+  }
+
+  h1 {
+    margin: 10% 0;
+
+    font-weight: normal;
+
+    font-size: 5rem;
+
+    color: var(--color-primary);
+  }
+
+  span {
+    display: block;
+  }
 }
 </style>
